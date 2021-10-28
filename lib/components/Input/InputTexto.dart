a0e7ec4b6841d7efd7ce15a33c6f7fbea2ebf396
JoaoVitorNatali <joao.vitornatali0@gmail.com>
2021-10-28 17:09:56 -0300

    Criando componente comum de texto

---
+++ lib/components/Input/InputTexto.dart
@@ -0,0 +1,61 @@
+import "package:flutter/material.dart";
+
+class InputTexto extends StatelessWidget {
+
+  late String texto_label;
+  late String texto_dica;
+  late bool password;
+  TextEditingController? controlador;
+  FormFieldValidator<String>? validator;
+  late TextInputType teclado;
+  FocusNode? marcador_foco;
+  FocusNode? recebedor_foco;
+
+
+  InputTexto(
+      this.texto_label,
+      {
+        this.texto_dica = "",
+        this.password = false,
+        this.controlador = null,
+        this.validator = null,
+        this.teclado = TextInputType.text,
+        this.marcador_foco = null,
+        this.recebedor_foco = null
+      }
+  ){
+    if(this.validator == null){
+      this.validator = (String? text){
+        if(text!.isEmpty)
+          return "O campo $texto_label é obrigatório";
+        return null;
+      };
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      validator: validator,
+      obscureText: password,
+      controller: controlador,
+      keyboardType: teclado,
+      textInputAction: TextInputAction.next,
+      focusNode: marcador_foco,
+      onFieldSubmitted: (String text){
+        FocusScope.of(context).requestFocus(recebedor_foco);
+      },
+      style: TextStyle(
+        fontSize: 25,
+        color: Colors.black,
+      ),
+      decoration: InputDecoration(
+        labelText: texto_label,
+        labelStyle: TextStyle(
+          fontSize: 25,
+          color: Colors.grey,
+        )
+      ),
+    );
+  }
+}
