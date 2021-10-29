1c45e645fed50a1ebad89c5409eea895b3cba05e
JoaoVitorNatali <joao.vitornatali0@gmail.com>
2021-10-28 23:01:29 -0300

    Realizando requisiçao para enviar codigo de acesso

--- lib/pages/Cadastro/formulario.dart
+++ lib/pages/Cadastro/formulario.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import '../../components/Input/InputTexto.dart';
 import 'dart:developer';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../../api/ResponseApi.dart';
 
 class FormularioCadastro extends StatefulWidget {
   const FormularioCadastro();
@@ -32,9 +35,39 @@ class _FormularioCadastroState extends State<FormularioCadastro> {
   String? validaTelefoneEmail(String? user){
     if(user!.isEmpty) return "O campo é obrigatório";
     else if(!validaTelefone(user) && !validaEmail(user)){
-      return "E-mail ou telefone inválidos";
+      return "E-mail ou telefone inválidos\nTelefone deve conter ddd (2 digitos) + 9 digitos";
     }
     else return null;
+  }
+
+  static Future<ApiResponse<String>> obterCodigoDeAcesso(access_pass) async {
+    try {
+      String _url = "https://api.olhai.me/v1/verifications";
+      Map params = {
+        'access_pass': access_pass,
+      };
+
+      var body = json.encode(params);
+
+      final _uri = Uri.parse(_url);
+      var response = await http.post(
+          _uri, body: body, headers: {"Content-Type": "application/json"});
+
+      if(response.statusCode == 200) return ApiResponse.ok(response.body);
+      return ApiResponse.error(response.body);
+    }
+    on Exception catch (erro){
+      return ApiResponse.error(erro.toString());
+    }
+  }
+
+  void realizarLogin(user){
+    String access_pass = user;
+    if(validaTelefone(user)){
+      access_pass = "+55" + user.replaceAll('(', '').replaceAll(')', '');
+    }
+
+    obterCodigoDeAcesso(access_pass);
   }
 
   @override
@@ -71,6 +104,7 @@ class _FormularioCadastroState extends State<FormularioCadastro> {
                 onPressed: (){
                   if(formkey.currentState?.validate() != null){
                     String user = controlador1.text;
+                    realizarLogin(user);
                   }
                 },
               ),
