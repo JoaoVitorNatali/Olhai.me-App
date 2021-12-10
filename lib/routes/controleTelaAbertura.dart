7db8d5f829614ae27f1f77f7e3f6d4bf3c3bdab3
JoaoVitorNatali <joao.vitornatali0@gmail.com>
2021-12-10 14:58:36 -0300

    Criando tela de splash

--- lib/routes/controleTelaAbertura.dart
+++ lib/routes/controleTelaAbertura.dart
@@ -1,8 +1,5 @@
-import 'dart:developer';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'package:shortlink/pages/Acessar/index.dart';
 import 'package:shortlink/preferences/User.dart';
 import 'package:shortlink/routes/index.dart';
@@ -15,33 +12,51 @@ class TelaAbertura extends StatefulWidget {
 }
 
 class _TelaAberturaState extends State<TelaAbertura> {
-  void iniciarAplicacao(){
 
-    Future<Map?> usuario = Usuario.obter();
-    
-    usuario.then((value) => {
-      if(value?["token"] != null){
-        Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (BuildContext context) => const Routes())
-        )
-      }
-      else{
-        Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (BuildContext context) => const Cadastro(title: 'Olhai.me'))
-        )
-      }
-    });
+  Future<Map?>? user;
+
+  Future<Map?> iniciarAplicacao() async{
+
+    return Usuario.obter();
+
+
+    // if(usuario?["token"] != null){
+    //   Navigator.pushReplacement(context,
+    //       MaterialPageRoute(builder: (BuildContext context) => const Routes())
+    //   );
+    // }
+    // else{
+    //   Navigator.pushReplacement(
+    //       context,
+    //       MaterialPageRoute(builder: (BuildContext context) => const Cadastro(title: 'Olhai.me'))
+    //   );
+    // }
+
   }
 
   @override
   void initState(){
     super.initState();
-    iniciarAplicacao();
+    user = iniciarAplicacao();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FutureBuilder(
+      future: user,
+      builder: (context, AsyncSnapshot snapshot){
+        if(!(snapshot.connectionState == ConnectionState.waiting)){
+          if(snapshot.data?["token"] != null){
+            return const Routes();
+          }
+          return const Cadastro(title: 'Olhai.me');
+        }
+        else{
+          return Container(
+            color: const Color(0xff5c5cb1),
+          );
+        }
+      }
+    );
   }
 }
